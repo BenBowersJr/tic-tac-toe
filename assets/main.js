e30f20730024887a35536c0423be888a50c1e3cc
Ben Bowers <benbowersjr.22@gmail.com>
2020-11-09 15:06:46 -0500

    basic code started

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,19 @@
 
+// start some variables up here
+let isXturn = true;
+let pEls = document.querySelectorAll('p')
+
+function startGame() {
+  // give each square a event listener
+    // if that event listener is used
+    //mark the square with the appropiate X or O
+    //go to next turn
+
+}
+
+function checkForWin() {
+  // we can check for wins like this
+  // have each grid have a id like a keyboard numpad.
+  // check if there is any win conditions by checking the squares ID's.
+  // if a player has squares 1, 4, 7 they win
+}
