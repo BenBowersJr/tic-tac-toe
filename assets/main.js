17b2c7cefabd69e7f9744f3d98b7f0e88125009b
Ben Bowers <benbowersjr.22@gmail.com>
2020-11-12 23:30:09 -0500

    changed how the game displays a winner

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,7 @@
 let randNum = Math.random()
 // select some DOM elements
 let isXTurn;
+let turnCount = 0;
 let p1 = document.querySelector('#one')
 let p2 = document.querySelector('#two')
 let p3 = document.querySelector('#three')
@@ -13,15 +14,7 @@ let p8 = document.querySelector('#eight')
 let p9 = document.querySelector('#nine')
 let turnEl = document.querySelector("#whosTurn")
 // these let us know if they moved in a square
-let moved1 = false;
-let moved2 = false;
-let moved3 = false;
-let moved4 = false;
-let moved5 = false;
-let moved6 = false;
-let moved7 = false;
-let moved8 = false;
-let moved9 = false;
+let moved1, moved2, moved3, moved4, moved5, moved6, moved7, moved8, moved9 = false;
 
 function startGame() {
   // give each square a event listener
@@ -46,7 +39,9 @@ function startGame() {
 }
 
 function checkForWin(turn) {
-  //check for all 8 win scenarios
+  turnCount++
+  console.log(turnCount)
+  //check for all 8 win scenarios, if win scenario detected, display winner
   if ((p1.textContent == turn && p2.textContent == turn && p3.textContent == turn) ||
   (p4.textContent == turn && p5.textContent == turn && p6.textContent == turn) ||
   (p7.textContent == turn && p8.textContent == turn && p9.textContent == turn) ||
@@ -55,29 +50,29 @@ function checkForWin(turn) {
   (p3.textContent == turn && p6.textContent == turn && p9.textContent == turn) ||
   (p1.textContent == turn && p5.textContent == turn && p9.textContent == turn) ||
   (p3.textContent == turn && p5.textContent == turn && p7.textContent == turn)) {
-    //if win scenario detected, display winner
     if (turn == "X") {
-      if(confirm('X wins! Restart?')){
-        window.location.reload();  
+      turnEl.textContent = "X wins!"
+      endGame()
+    } else if (turn == "O") {
+      turnEl.textContent = "O wins!"
+      endGame()
     }
-    } else {
-      if(confirm('O wins! Restart?')){
-        window.location.reload();  
-    }
-    }
+    //if it is a draw
+  } else if (turnCount == 9) {
+    turnEl.textContent = "Draw!"
+    endGame()
   }
 }
 
 function move1() {
-  // if not moved yet and its X's turn
+  // if they havent moved here yet and its x's turn
   if (!moved1 && isXTurn) {
-    // Do X turn stuff
     p1.textContent = 'X'
     isXTurn = false;
     turnEl.textContent = "It is O's Turn.";
     checkForWin("X");
+  // if they havent moved here yet and its o's turn
   } else if (!moved1 && !isXTurn) {
-    // do O turn stuff otherwise
     p1.textContent = 'O';
     isXTurn = true;
     turnEl.textContent = "It is X's Turn.";
@@ -230,6 +225,17 @@ function move9() {
   // this makes it so they cant move in this square anymore
   moved9 = true;
 }
-
+function endGame() {
+  //remove all listeners
+  p1.removeEventListener('click', move1)
+  p2.removeEventListener('click', move2)
+  p3.removeEventListener('click', move3)
+  p4.removeEventListener('click', move4)
+  p5.removeEventListener('click', move5)
+  p6.removeEventListener('click', move6)
+  p7.removeEventListener('click', move7)
+  p8.removeEventListener('click', move8)
+  p9.removeEventListener('click', move9)
+}
 
 startGame();
